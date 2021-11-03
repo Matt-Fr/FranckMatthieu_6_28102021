6055c14565b235c9ce3add337310106c98e71337
Matt-Fr <80825384+Matt-Fr@users.noreply.github.com>
2021-11-03 13:57:03 +0100

    initial commit

---
+++ script.js
@@ -0,0 +1,17 @@
+// function photographFactory(name, city, tagline, price, tags) {
+//   return {
+//     name: name,
+//     city: city,
+//     tagline: tagline,
+//     price: price,
+//     tags: tags,
+//   };
+// }
+
+function PhotographFactory(name, city, tagline, price, tags) {
+  this.name = name;
+  this.city = city;
+  this.tagline = tagline;
+  this.price = price;
+  this.tags = tags;
+}
