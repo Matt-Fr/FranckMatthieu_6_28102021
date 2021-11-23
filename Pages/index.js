5ef5f564be9d337498d78af9325e4839bf7e6944
Matt-Fr <80825384+Matt-Fr@users.noreply.github.com>
2021-11-23 18:05:32 +0100

    fetch photographers

--- Pages/index.js
+++ Pages/index.js
@@ -1,97 +1,28 @@
-const photographers = [
-  {
-    name: "Mimi Keel",
-    id: 243,
-    city: "London",
-    country: "UK",
-    tags: ["portrait", "events", "travel", "animals"],
-    tagline: "Voir le beau dans le quotidien",
-    price: 400,
-    portrait: "MimiKeel.jpg",
-  },
-  {
-    name: "Ellie-Rose Wilkens",
-    id: 930,
-    city: "Paris",
-    country: "France",
-    tags: ["sports", "architecture"],
-    tagline: "Capturer des compositions complexes",
-    price: 250,
-    portrait: "EllieRoseWilkens.jpg",
-  },
-  {
-    name: "Tracy Galindo",
-    id: 82,
-    city: "Montreal",
-    country: "Canada",
-    tags: ["art", "fashion", "events"],
-    tagline: "Photographe freelance",
-    price: 500,
-    portrait: "TracyGalindo.jpg",
-  },
-  {
-    name: "Nabeel Bradford",
-    id: 527,
-    city: "Mexico City",
-    country: "Mexico",
-    tags: ["travel", "portrait"],
-    tagline: "Toujours aller de l'avant",
-    price: 350,
-    portrait: "NabeelBradford.jpg",
-  },
-  {
-    name: "Rhode Dubois",
-    id: 925,
-    city: "Barcelona",
-    country: "Spain",
-    tags: ["sport", "fashion", "events", "animals"],
-    tagline: "Je crée des souvenirs",
-    price: 275,
-    portrait: "RhodeDubois.jpg",
-  },
-  {
-    name: "Marcel Nikolic",
-    id: 195,
-    city: "Berlin",
-    country: "Germany",
-    tags: ["travel", "architecture"],
-    tagline: "Toujours à la recherche de LA photo",
-    price: 300,
-    portrait: "MarcelNikolic.jpg",
-  },
-];
+async function getPhotographers() {
+  const response = await fetch("./photographers.json");
+  const data = await response.json();
+  const photographers = data.photographers;
 
-fetch("./photographers.json")
-  .then((response) => response.json())
-  .then((data) => console.log(data.photographers))
-  .catch((error) => console.log(error));
+  // et bien retourner le tableau photographers seulement une fois
+  return {
+    photographers: [...photographers],
+  };
+}
 
-const section = document.querySelector(".photograph-container");
+async function displayData(photographers) {
+  const photographersSection = document.querySelector(".photograph-container");
 
-window.addEventListener("DOMContentLoaded", function () {
-  let displayPhotographer = photographers.map(function (item) {
-    return `<article class="photograph">
-        <a href="" class="photograph-link">
-          <img
-            src="./Sample Photos/Photographers ID Photos/${item.portrait}"
-            alt=""
-            class="photograph-img"
-          />
-          <h2 class="photograph-name">${item.name}</h2>
-        </a>
-        <div class="information-container">
-          <p class="city">${item.city},${item.country}</p>
-          <p class="tagline">${item.tagline}</p>
-          <p class="price">${item.price}€/jour</p>
-        </div>
-        <div class="photograph-tag-container">
-          <button class="tag">#Sometag</button>
-          <button class="tag">#Sometag</button>
-          <button class="tag">#Sometag</button>
-          <button class="tag">#Sometag</button>
-        </div>
-      </article>`;
+  photographers.forEach((photographer) => {
+    const photographerModel = photographerFactory(photographer);
+    const userCardDOM = photographerModel.getUserCardDOM();
+    photographersSection.appendChild(userCardDOM);
   });
-  displayPhotographer = displayPhotographer.join("");
-  section.innerHTML = displayPhotographer;
-});
+}
+
+async function init() {
+  // Récupère les datas des photographes
+  const { photographers } = await getPhotographers();
+  displayData(photographers);
+}
+
+init();
--- factory/photographer.js
+++ factory/photographer.js
@@ -1,16 +1,46 @@
 function photographerFactory(data) {
-  const { name, portrait } = data;
+  const { name, portrait, city, tagline, price } = data;
 
-  const picture = `assets/photographers/${portrait}`;
+  const picture = `Sample Photos/Photographers ID Photos/${portrait}`;
 
   function getUserCardDOM() {
     const article = document.createElement("article");
+    article.className = "photograph";
+    const link = document.createElement("a");
+    link.className = "photograph-link";
+
     const img = document.createElement("img");
     img.setAttribute("src", picture);
+    img.className = "photograph-img";
     const h2 = document.createElement("h2");
     h2.textContent = name;
-    article.appendChild(img);
-    article.appendChild(h2);
+    h2.className = "photograph-name";
+
+    const divInfo = document.createElement("div");
+    divInfo.className = "information-container";
+    const parCity = document.createElement("p");
+    parCity.className = "city";
+    parCity.textContent = city;
+
+    const parTagline = document.createElement("p");
+    parTagline.className = "tagline";
+    parTagline.textContent = tagline;
+
+    const parPrice = document.createElement("p");
+    parPrice.className = "price";
+    parPrice.innerHTML = `${price}€/jour`;
+
+    const divTag = document.createElement("div");
+    divTag.className = "photograph-tag-container";
+
+    article.appendChild(link);
+    link.appendChild(img);
+    link.appendChild(h2);
+    article.appendChild(divInfo);
+    divInfo.appendChild(parCity);
+    divInfo.appendChild(parTagline);
+    divInfo.appendChild(parPrice);
+
     return article;
   }
   return { name, picture, getUserCardDOM };
